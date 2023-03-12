3df713800a1cd7d313195ed9605d2ad596d8d35f
Shrirang Kahale <67057319+Albonycal@users.noreply.github.com>
2023-03-12 11:03:50 +0530

    fixed env var ONTPASS
    typo fixed

--- scripts/get_uptime.sh
+++ scripts/get_uptime.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sshpass -p $SSHPASS ssh -o StrictHostKeyChecking=no ONTUSER@192.168.1.1 uptime |  cut -d ' ' -f2
+sshpass -p $ONTPASS ssh -o StrictHostKeyChecking=no ONTUSER@192.168.1.1 uptime |  cut -d ' ' -f2
 
