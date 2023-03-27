ea08149f0ca14d2d357b191b28396ddbadc50f3a
Albonycal <albonycal@gmail.com>
2023-03-27 14:18:33 +0530

    ONTDash v0.3 Added comments

--- scripts/get_bandwidth_usage.sh
+++ scripts/get_bandwidth_usage.sh
@@ -1,5 +1,10 @@
 # ONT BANDWIDTH
+# This downloads the script responsible for the bandwidth monitor. For some mysterious reasons the built in curl / wget
+# Don't work properly with HTTPS, so I am hosting it on my mirror, feel free to check the code, its fairly simple 
 #
+
 sshpass -p $ONTPASS ssh ONTUSER@192.168.1.1 'curl -s -L http://mirror.albony.xyz/net.sh -o /configs/net.sh' > /dev/null
+
+# pon_100_0_1 is the interface through which all the traffic to WAN goes 
 sshpass -p $ONTPASS ssh ONTUSER@192.168.1.1 sh /configs/net.sh pon_100_0_1
 
